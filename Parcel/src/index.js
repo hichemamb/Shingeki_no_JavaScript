d4581482abadf327060fb04b353948680a32d9a6
AMAR BENSABER Hichem <hichemamb@MacBook-Pro-de-AMAR.local>
2018-05-21 10:29:34 -0700

    added

--- Parcel/src/index.js
+++ Parcel/src/index.js
@@ -2,14 +2,27 @@ import 'flexboxgrid';
 import './style/style.scss';
 
 
-var one = document.querySelector('.Header-one');
+
 var two = document.querySelector('.Header-two');
+var three = document.querySelector('.Header-three');
+var four = document.querySelector('.Header-four');
 var head = document.querySelector('.Header');
 
 
-document.addEventListener('mouseover',function(){
-  one.style.left="100px";
-  two.style.right="100px";
-  one.style.transition="all 2s ease";
+window.addEventListener('load',function(){
+  two.style.right="0px";
   two.style.transition="all 2s ease";
+  setTimeout(function(){
+  three.style.right="0px";
+  three.style.transition="all 3s ease"
+  four.style.right="0px";
+  four.style.transition="all 5s ease"
+   }, 150);
+
+   two.style.width="750px";
+   two.style.transition="all 2s ease";
+   three.style.opacity="1";
+   three.style.transition="all 10s";
+   four.style.opacity="1";
+   four.style.transition="all 10s";
 })
